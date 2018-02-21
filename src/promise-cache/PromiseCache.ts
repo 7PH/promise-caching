9497b3a5f777654a48959ac5fe33ee47608a9fd8
7PH <b.raymond@protonmail.com>
2018-02-21 23:47:08 +0100

    Initial commit

---
+++ src/promise-cache/PromiseCache.ts
@@ -0,0 +1,86 @@
+
+export interface CachedObject {
+    state: 'generating' | 'generated';
+    created: number;
+    unresolved: ((d: any) => any)[];
+    data: any;
+}
+
+export class PromiseCache {
+
+    private static readonly cached: Map<string, CachedObject> = new Map();
+
+    private static readCache(key: string): CachedObject | undefined {
+        return PromiseCache.cached.get(key);
+    }
+
+    private static regenCache<T>(key: string, expire: number, generator: () => Promise<T>): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            // init new cache that will be generated
+            const cache: CachedObject = {
+                state: 'generating',
+                created: 0,
+                unresolved: [],
+                data: null
+            };
+            PromiseCache.cached.set(key, cache);
+
+            // generated it
+            generator()
+                .then((data: T) => {
+
+                    // updates cache data
+                    cache.state = 'generated';
+                    cache.created = Date.now();
+                    cache.data = data;
+
+                    // resolve pending promises
+                    for (let i = 0; i < cache.unresolved.length; ++ i)
+                        cache.unresolved[i](data);
+
+                    // trigger the cache deletion
+                    setTimeout(() => {
+                        PromiseCache.cached.delete(key);
+                    }, expire);
+
+                    // resolves this Promise
+                    resolve(data);
+
+                }).catch((d: any) => {
+                PromiseCache.cached.delete(key);
+
+                reject(d);
+            });
+        });
+    }
+
+    public static get<T>(key: string, expire?: number, generator?: () => Promise<T>): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            let cache: CachedObject | undefined = PromiseCache.readCache(key);
+
+            if (cache == null) {
+                if (generator != null) {
+                    // SHOULD and CAN generate cache
+                    return PromiseCache.regenCache(key, expire || -1, generator)
+                        .then((data: T) => {
+                            resolve(data);
+                        }).catch((d: any) => {
+                            reject(d);
+                        });
+                } else {
+                    // SHOULD but CANNOT generate cache
+                    if (cache != null) {
+                        // Give expired cache, that is the best we can do
+                        return resolve();
+                    } else {
+                        return reject(new Error("Cache does not exists and generator was not provided"));
+                    }
+                }
+            } else if (cache.state === 'generating') {
+                cache.unresolved.push(resolve);
+            } else {
+                return resolve(cache.data);
+            }
+        });
+    }
+}
