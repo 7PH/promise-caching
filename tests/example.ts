db8fe28d899159a196310bc981d810ab8919b27f
7PH <b.raymond@protonmail.com>
2018-07-07 16:26:41 +0200

    Update documentation

--- tests/example.ts
+++ tests/example.ts
@@ -1,21 +1,3 @@
-# Promise-Caching
-
-Let's suppose you have a high cost function that returns a promise. This library allows you to:
-
-- cache its result for a specified amount of time
-- decide whether you want to return the expired cache or force regeneration when it expires
-
-## Time chart
-
-Here is a chart of what's happening when you set 'returnExpired' to false
-
-![Time chart](/doc/time-chart.png?raw=true "Time chart")
-
-When you set 'returnExpired' to true, retrieving an expired promise won't hang. Instead, it will return the expired promise and regenerate a new result in background.
-
-## Usage
-
-```typescript
 import {PromiseCaching} from "../src";
 
 
@@ -56,4 +38,3 @@ let cache: PromiseCaching = new PromiseCaching({ returnExpired: true });
     //      the promise will be resolved when the new generation is done
 
 })();
-```
