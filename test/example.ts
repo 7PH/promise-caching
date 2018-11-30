af7704f8a104d9707e1555b438416e5b658141ad
PowerHat <b.raymond@protonmail.com>
2018-11-30 11:51:15 +0100

    Simplified example in README

--- test/example.ts
+++ test/example.ts
@@ -1,20 +1,16 @@
 import {PromiseCaching} from "../src";
 
-
+// asynchronous sleep function
 async function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve.bind(this), ms));
 }
 
-// high cost function
+// the high cost function
 async function getRandom() {
     await sleep(1000);
     return Math.random();
 }
 
-// cached function
-function getRandomCached(key: any) {
-    return cache.get(key, 2000, getRandom);
-}
 // your cache instance
 let cache: PromiseCaching = new PromiseCaching({ returnExpired: true });
 
@@ -22,18 +18,18 @@ let cache: PromiseCaching = new PromiseCaching({ returnExpired: true });
 (async () => {
 
     // cache did not exist. this call takes 1 second
-    await getRandomCached('random');
+    await cache.get('random', 2000, getRandom);
 
     // starting from now, cache will be valid 2 seconds
 
     // this call will be instantaneous
-    await getRandomCached('random');
+    await cache.get('random', 2000, getRandom);
 
     // we wait for the cache to expire
     await sleep(2000);
 
     // the cache has expired now
-    await getRandomCached('random');
+    await cache.get('random', 2000, getRandom);
 
     // if 'returnExpired' is true,
     //      the value will be instantaneously returned and the cache
@@ -45,6 +41,6 @@ let cache: PromiseCaching = new PromiseCaching({ returnExpired: true });
     // you can use whatever key you want for caching
     let key: any = {a: 1};
 
-    await getRandomCached(key);
-    await getRandomCached(key);
+    await cache.get(key, 2000, getRandom);
+    await cache.get(key, 2000, getRandom);
 })();
